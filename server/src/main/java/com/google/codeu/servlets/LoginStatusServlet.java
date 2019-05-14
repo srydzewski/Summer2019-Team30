9c34b1b427d49fdb18673ef7b09aa335f8e62009
Brian Chen <brianyiingchen@gmail.com>
2019-05-14 00:52:39 -0700

    Refactored the codebase to more accurately match the original code u starter guide.

--- server/src/main/java/com/google/codeu/servlets/LoginStatusServlet.java
+++ server/src/main/java/com/google/codeu/servlets/LoginStatusServlet.java
@@ -1,6 +1,22 @@
-package com.github.whaleshop.servlets;
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package com.google.codeu.servlets;
 
-import com.github.whaleshop.proto.LoginStatus;
+import com.google.codeu.proto.LoginStatus;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.protobuf.util.JsonFormat;
@@ -17,7 +33,6 @@ public class LoginStatusServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    log.info("doGet()");
     UserService userService = UserServiceFactory.getUserService();
     boolean isLoggedIn = userService.isUserLoggedIn();
 
