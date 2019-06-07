cda4253ba828db6becd5d90228e8a6c473d20dc0
Alex Kim <50557844+AlexK81799@users.noreply.github.com>
2019-06-07 15:05:33 -0400

    Display a embedded Google Map on the Home page (#68)
    Added an embedded google map to the homepage of Googleplex

--- client/src/components/page/Home.js
+++ client/src/components/page/Home.js
@@ -15,12 +15,17 @@
  */
 
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const GOOGLE_MAPS_APP_KEY = { key: 'AIzaSyAi9TMtkY74gzfmjPkD7w1Tu-zyABHYlww' };
+const GOOGLEPLEX_COORD = { lat: 37.422, lng: -122.084 };
+const DEFAULT_MAP_ZOOM = 16;
 
 /** Renders the /home page. */
 class Home extends Component {
   render() {
     return (
-      <div className='container'>
+      <div className='container' style={{ height: '25vh', width: '100%' }}>
         <h1 className='center'>CodeU Starter Project</h1>
         <p>
           This is the CodeU starter project. Click the links above to login and
@@ -32,6 +37,14 @@ class Home extends Component {
           afraid to get creative! You could start by modifying this page to tell
           the world more about your team.
         </p>
+        <p>
+          Below you will find a map of Googleplex in Mountain View, California.
+        </p>
+        <GoogleMapReact
+          bootstrapURLKeys={GOOGLE_MAPS_APP_KEY}
+          defaultCenter={GOOGLEPLEX_COORD}
+          defaultZoom={DEFAULT_MAP_ZOOM}
+        />
       </div>
     );
   }
