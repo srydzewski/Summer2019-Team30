bc36f22f7f7f3dec47780cebee71f97bf1435efe
anthonyedwards <anthonydwrds8@gmail.com>
2019-06-29 20:25:29 -0400

    Attempting to merge commits

--- server/src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
+++ server/src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
@@ -1,12 +1,12 @@
 @WebServlet("/blobstore-upload-url")
 public class BlobstoreUploadUrlServlet extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler");
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler");
 
-        response.setContentType("text/html");
-        response.getOutputStream().println(uploadUrl);
-    }
+    response.setContentType("text/html");
+    response.getOutputStream().println(uploadUrl);
+  }
 }
--- server/src/main/java/com/google/codeu/servlets/HomeServlet.java
+++ server/src/main/java/com/google/codeu/servlets/HomeServlet.java
@@ -2,15 +2,15 @@
 public class HomeServlet extends HttpServlet {
 
   /**
-   * Returns HTML that contains a form. The form submits to Blobstore,
-   * which redirects to our /my-form-handler, which is handled by FormHandlerServlet.
+   * Returns HTML that contains a form. The form submits to Blobstore, which redirects to our
+   * /my-form-handler, which is handled by FormHandlerServlet.
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the Blobstore URL
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler") ;
+    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler");
 
     response.setContentType("text/html");
 
@@ -18,7 +18,8 @@ public class HomeServlet extends HttpServlet {
     // This is not how you'd do this in a real codebase!
     // See the hello-world-jsp or hello-world-fetch examples for more info.
     ServletOutputStream out = response.getOutputStream();
-    out.println("<form method=\"POST\" enctype=\"multipart/form-data\" action=\"" + uploadUrl + "\">");
+    out.println(
+        "<form method=\"POST\" enctype=\"multipart/form-data\" action=\"" + uploadUrl + "\">");
 
     out.println("<p>Type some text:</p>");
     out.println("<textarea name=\"message\"></textarea>");
