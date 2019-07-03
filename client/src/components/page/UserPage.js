6d1bbb94ff1cbccb15b30d95ec453274da895bc7
Samantha Rydzewski <srseco28@gmail.com>
2019-07-02 20:58:15 -0400

    New User Page Layout
    Changed layout and added place for profile picture
    
    Issue: https://github.com/fluffysheep-codeu/Summer2019-Team30/issues/84
    PR: https://github.com/fluffysheep-codeu/Summer2019-Team30/pull/97

--- client/src/components/page/UserPage.js
+++ client/src/components/page/UserPage.js
@@ -25,6 +25,11 @@ import Message from 'components/ui/Message.js';
 import { ABOUT_ME_SERVLET } from '../../constants/links';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 /** Gets the parameters from the url. Parameters are after the ? in the url. */
 const urlParams = new URLSearchParams(window.location.search);
@@ -40,6 +45,30 @@ const promises = Promise.all([fetch(url1), fetch(url2)]);
 var editorMessage = null;
 /* User-Entered About */
 var editorAbout = 'This is your about me.';
+
+const styles = function() {
+  return {
+    a: {
+      marginLeft: 50,
+      marginTop: 10,
+      width: 100,
+      height: 100,
+      justify: 'center'
+    },
+    submit: {
+      marginTop: 10
+    },
+    words: {
+      fontSize: 18,
+      marginLeft: 8
+    },
+    header: {
+      fontSize: 35,
+      marginTop: 30,
+      fontFamily: 'PT Sans'
+    }
+  };
+};
 
 /**
  * @param message A message sent from a user with a timestamp.
@@ -97,6 +126,7 @@ class UserPage extends Component {
     const { messages, about } = this.state;
 
     const { userEmail } = this.props.userData;
+    const { classes } = this.props;
 
     // A boolean that checks whether the current logged in user is viewing
     // another user's page. Some controls such as the message form will hide if
@@ -112,29 +142,67 @@ class UserPage extends Component {
     const aboutUi = about ? about.content : null;
 
     return (
-      <div className='container' style={{ margin: 5 }}>
-        <h1 className='center'>{userEmailParam}</h1>
-        Enter a new message:
+      <div className='container' style={{ margin: 10 }}>
+        <Grid container spacing={10}>
+          <Grid item xs={2}>
+            <Avatar
+              className={classes.a}
+              alt='My profile'
+              src='https://cc-media-foxit.fichub.com/image/fox-it-mondofox/e8c0f288-781d-4d0b-98ad-fd169782b53b/scene-sottacqua-per-i-sequel-di-avatar-maxw-654.jpg'
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Typography className={classes.header}>{userEmailParam}</Typography>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            style={{ height: 30, marginTop: 10, marginLeft: 60 }}>
+            <div className={hiddenIfViewingOther}>
+              <CKEditor
+                editor={ClassicEditor}
+                onInit={editor => {}}
+                onChange={(event, editor) => {
+                  editorAbout = editor.getData();
+                }}
+              />
+              <Button
+                onClick={submitAboutMe}
+                className={classes.submit}
+                variant='contained'
+                color='primary'>
+                Submit
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={2} />
+          <Grid item xs={3}>
+            {aboutUi}
+          </Grid>
+        </Grid>
         <br />
-        <CKEditor
-          editor={ClassicEditor}
-          onInit={editor => {}}
-          onChange={(event, editor) => {
-            editorMessage = editor.getData();
-          }}
-        />
-        <button onClick={submitMessage}>Submit</button>
+        <div className={hiddenIfViewingOther}>
+          <Typography variant='h6' className={classes.words}>
+            Enter a new message:
+          </Typography>
+          <CKEditor
+            editor={ClassicEditor}
+            onInit={editor => {}}
+            onChange={(event, editor) => {
+              editorMessage = editor.getData();
+            }}
+          />
+          <Button
+            onClick={submitMessage}
+            className={classes.submit}
+            color='primary'
+            variant='contained'>
+            Submit
+          </Button>
+        </div>
         <br />
-        {aboutUi}
-        <CKEditor
-          editor={ClassicEditor}
-          onInit={editor => {}}
-          onChange={(event, editor) => {
-            editorAbout = editor.getData();
-          }}
-        />
-        <button onClick={submitAboutMe}>Submit</button>
-        <br className={hiddenIfViewingOther} />
         <hr />
         <p className={hiddenIfHasMessages}>This user has no posts yet.</p>
         {messagesUi}
@@ -145,7 +213,9 @@ class UserPage extends Component {
 
 UserPage.propTypes = {
   /** A json of the user data. */
-  userData: PropTypes.object
+  userData: PropTypes.object,
+  /**required by material-ui */
+  classes: PropTypes.object.isRequired
 };
 
 /** Maps user data from redux to UserPage. */
@@ -153,4 +223,4 @@ const mapStateToProps = function(state) {
   return { userData: state.userData };
 };
 
-export default connect(mapStateToProps)(UserPage);
+export default connect(mapStateToProps)(withStyles(styles)(UserPage));
