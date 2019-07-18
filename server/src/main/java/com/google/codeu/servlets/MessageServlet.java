851cc1e5beac2cc6ad570bb1f6e9979b072e38b8
anthonyedwards <anthonydwrds8@gmail.com>
2019-07-18 15:43:40 -0500

    something to rebase

--- server/src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ server/src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -16,6 +16,20 @@
 
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+import java.util.regex.*;
+
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
@@ -36,14 +50,18 @@ public class MessageServlet extends HttpServlet {
 
   private Datastore datastore;
 
+  private final static String imageReplacement = "<img src=\"$1\" />";
+  private final static String videoReplacement = "<video controls><source src=\"$1\" type=\"video/webm\"><source src = \"$1\" type = \"video/mp4\"></video>";
+  private final static String audioReplacement = "<audio controls><source src=\"$1\" type=\"audio/mp3\"><source src = \"$1\" type = \"audio/wav\"></audio>";
+
   @Override
   public void init() {
     datastore = new Datastore();
   }
 
   /**
-   * Responds with a JSON representation of {@link Message} data for a specific user. Responds with
-   * an empty array if the user is not provided.
+   * Responds with a JSON representation of {@link Message} data for a specific
+   * user. Responds with an empty array if the user is not provided.
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -76,13 +94,190 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
 
-    String regex = "(https?://\\S+\\.(png|jpg))";
-    String replacement = "<img src=\"$1\" />";
-    String textWithImagesReplaced = userText.replaceAll(regex, replacement);
+    /* replace file urls with corresponding html tags(<img>, <video>, <audio>) */
+    String textReplaced = tagURLs(userText);
 
-    Message message = new Message(user, textWithImagesReplaced);
+    /*
+     * adding image tags and corresponding labels for the image file uploaded to
+     * Blobstore to the end of message
+     */
+    String messageText = textReplaced;
+    List<BlobKey> imageBlobKeys = getBlobKeys(request, "image");
+    /* get image url ulpoaded to Blobstore */
+    List<String> imageBlobUrls = getUploadedFileUrl(imageBlobKeys);
+    /* add image tag for uploaded image url at the end of message text */
+    if (imageBlobUrls != null && !imageBlobUrls.isEmpty()) {
+      for (int i = 0; i < imageBlobUrls.size(); i++) {
+        // add image tag for the image user uploaded to Blobstore
+        messageText += "<img src=\"" + imageBlobUrls.get(i) + "\" />";
+
+      }
+    }
+
+    /* storing the message in Datastore */
+    Message message = new Message(user, messageText);
     datastore.storeMessage(message);
 
     response.sendRedirect("/userpage?user=" + user);
   }
+
+  /**
+   * Returns the BlobKey that points to the image files uploaded by the user, or
+   * null if the user didn't upload any image file.
+   */
+  private List<BlobKey> getBlobKeys(HttpServletRequest request, String formInputElementName) {
+    List<BlobKey> imageBlobKeys = new ArrayList<>();
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get(formInputElementName);
+
+    // User submitted form without selecting a file, so we can't get a URL.
+    // (devserver)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // User submitted form without selecting a file, so we can't get a URL. (live
+    // server)
+    BlobKey blobKey = blobKeys.get(0);
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // selecting blobKeys for image blobs only
+    for (BlobKey blobK : blobKeys) {
+      // Checking the validity of the file to make sure it's an image
+      String fileType = new BlobInfoFactory().loadBlobInfo(blobK).getContentType().toLowerCase();
+      if (!(fileType.equals("image/jpg") || fileType.equals("image/jpeg") || fileType.equals("image/gif")
+          || fileType.equals("image/png"))) {
+        blobstoreService.delete(blobK);
+      } else // blob is an image file
+      {
+        imageBlobKeys.add(blobK);
+      }
+    }
+    return imageBlobKeys;
+  }
+
+  /**
+   * Returns a list URL that points to the uploaded image files in Blobstore
+   */
+  private List<String> getUploadedFileUrl(List<BlobKey> imageBlobKeys) {
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    List<String> imageBlobUrls = new ArrayList<>();
+
+    if (imageBlobKeys != null) {
+      for (BlobKey blobK : imageBlobKeys) {
+        try {
+          // getting the image URL to the uploaded file
+          ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobK);
+          String imageUrl = imagesService.getServingUrl(options);// getServingUrl locks the blob, so it cannot be
+                                                                 // deleted with blobstoreService.delete(blobK);
+          imageBlobUrls.add(imageUrl);
+        } catch (IllegalArgumentException e) {
+          System.out.println(e.getMessage());
+        }
+      }
+    }
+
+    return imageBlobUrls;
+  }
+
+  /**
+   * Replaces valid image url in the message with image tag Does not change the
+   * strig message if there are no valid image urls
+   */
+  public String tagURLs(String message) {
+    String regex = "(https?://\\S*?\\.(?:png|PNG|html5|jpg|JPG|jpeg|JPEG|gif|GIF|mp4|MP4|webm|mp3|MP3|wav|WAV))";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(message);
+
+    // creating list of all urls that match the pattern
+    List<String> urls = new ArrayList<String>();
+    int firstMarchedUrlIndex = -1;
+    // adding first matched url and getting its index
+    if (matcher.find()) {
+      String matchedUrl = matcher.group(1);
+      firstMarchedUrlIndex = matcher.start();
+      urls.add(matchedUrl);
+    }
+    // adding other matched urls
+    while (matcher.find()) {
+      String matchedUrl = matcher.group(1);
+      urls.add(matchedUrl);
+    }
+
+    // creating list of non-url sections of text
+    String[] nonUrlTexts = message.split(regex);
+
+    // checking if url is valid Using Java library
+    for (int i = 0; i < urls.size(); i++) {
+      boolean isUrlValid = false;
+      try {
+
+        URI uri = new URL(urls.get(i)).toURI();
+        uri.parseServerAuthority();
+        isUrlValid = true;
+      } catch (Exception e) {
+      }
+
+      if (isUrlValid) {
+        if (urls.get(i).toLowerCase().endsWith("mp4") || urls.get(i).toLowerCase().endsWith("webm"))
+          urls.set(i, urls.get(i).replaceAll(regex, videoReplacement));
+        else if (urls.get(i).toLowerCase().endsWith("mp3") || urls.get(i).toLowerCase().endsWith("wav"))
+          urls.set(i, urls.get(i).replaceAll(regex, audioReplacement));
+        else
+          urls.set(i, urls.get(i).replaceAll(regex, imageReplacement));
+      }
+    }
+
+    // putting urls and nonUrlTexts back into one single message
+    message = "";
+    if (firstMarchedUrlIndex == 0) {
+      for (int i = 0; i < urls.size(); i++) {
+        message += urls.get(i);
+        if (i + 1 < nonUrlTexts.length)
+          message += nonUrlTexts[i + 1];
+      }
+    } else {
+      for (int i = 0; i < nonUrlTexts.length; i++) {
+        message += nonUrlTexts[i];
+        if (i < urls.size())
+          message += urls.get(i);
+      }
+    }
+
+    return message;
+  }
+
+  /**
+   * Blobstore stores files as binary data. This function retrieves the binary
+   * data stored at the BlobKey parameter. return blob as byte array
+   */
+  private byte[] getBlobBytes(BlobKey blobKey) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    ByteArrayOutputStream outputBytes = new ByteArrayOutputStream();
+
+    int fetchSize = BlobstoreService.MAX_BLOB_FETCH_SIZE;
+    long currentByteIndex = 0;
+    boolean continueReading = true;
+    while (continueReading) {
+      // end index is inclusive, so we have to subtract 1 to get fetchSize bytes
+      byte[] b = blobstoreService.fetchData(blobKey, currentByteIndex, currentByteIndex + fetchSize - 1);
+      outputBytes.write(b);
+
+      // if we read fewer bytes than we requested, then we reached the end
+      if (b.length < fetchSize) {
+        continueReading = false;
+      }
+
+      currentByteIndex += fetchSize;
+    }
+
+    return outputBytes.toByteArray();
+  }
+
 }
